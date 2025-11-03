529ade40a6911c8f699af162eb7ddf2741b45ef9
PuneethKal <puneeth.kallem@gmail.com>
2025-11-03 15:16:56 -0500

    created weather API

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ dotenv.config({path:"../.env"});
 const app = express();
 app.use(cors());
 
+app.get("/api/weather", async (req, res) => {
+
+    console.log(req);
+
+    console.log("Calling Weather API ...");
+    // const TorontoLOC = {lat:'43.651070', lon:'-79.347015'}
+    // const city = req.query.city || TorontoLOC; // city should be json with lat and lon 
+    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${process.env.OpenWeatherMapAPI}`;
+    // const { data } = await axios.get(url);
+    // res.json(data);
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

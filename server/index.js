cae023ba87bfe0c619ca54416417cdd120e01c4f
PuneethKal <puneeth.kallem@gmail.com>
2025-11-03 15:39:01 -0500

    added forecast api

--- server/index.js
+++ server/index.js
@@ -8,14 +8,31 @@ const app = express();
 app.use(cors());
 app.use(express.json()) // Enables reading body of req
 
+const TorontoLOC = {lat:'43.651070', lon:'-79.347015'}
+
+// Current weather API
 app.get("/api/weather", async (req, res) => {
     console.log("Calling Weather API ...");
-    const TorontoLOC = {lat:'43.651070', lon:'-79.347015'}
-    const city = req.body.city || TorontoLOC; // city should be json with lat and lon 
+    const city = req.body.city || TorontoLOC;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${process.env.OpenWeatherMapAPI}`;
     const { data } = await axios.get(url);
     res.json(data);
 });
+
+// 5 day / 3 hour forecast API
+app.get("/api/forecast", async (req, res) => {
+    console.log("Calling Weather API ...");
+    const city = req.body.city || TorontoLOC;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=${process.env.OpenWeatherMapAPI}`;
+    const { data } = await axios.get(url);
+    res.json(data);
+});
+
+//Weather Maps (current) api
+
+//Air Pollution api
+
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

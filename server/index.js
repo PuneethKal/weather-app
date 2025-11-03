8d84a3b20869428c1d67cacbf06b5710adec7526
PuneethKal <puneeth.kallem@gmail.com>
2025-11-03 15:32:05 -0500

    updated weather api to accept lon/lat

--- server/index.js
+++ server/index.js
@@ -6,17 +6,15 @@ import dotenv from 'dotenv';
 dotenv.config({path:"../.env"});
 const app = express();
 app.use(cors());
+app.use(express.json()) // Enables reading body of req
 
 app.get("/api/weather", async (req, res) => {
-
-    console.log(req);
-
     console.log("Calling Weather API ...");
-    // const TorontoLOC = {lat:'43.651070', lon:'-79.347015'}
-    // const city = req.query.city || TorontoLOC; // city should be json with lat and lon 
-    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${process.env.OpenWeatherMapAPI}`;
-    // const { data } = await axios.get(url);
-    // res.json(data);
+    const TorontoLOC = {lat:'43.651070', lon:'-79.347015'}
+    const city = req.body.city || TorontoLOC; // city should be json with lat and lon 
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${process.env.OpenWeatherMapAPI}`;
+    const { data } = await axios.get(url);
+    res.json(data);
 });
 
 const PORT = 5000;
